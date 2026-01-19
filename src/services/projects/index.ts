1c2e11be72105b9b1cfa95f29923829e0bf94c79
stvnCodes <estivenposadarua@gmail.com>
2026-01-19 12:10:50 -0500

    detalles en el index

--- src/services/projects/index.ts
+++ src/services/projects/index.ts
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
+
 export { projectsService } from './projects.service';
-=======
+
 export * from './projects.service';
 export * from './membership.service';
 export * from './invitations.service';
->>>>>>> e559fb7663e4e165d2799e63dc6043415bf2a55d
